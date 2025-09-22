61ff34d3ffc9d9b65583d62004e3b1d4180648c0
Tkaki-W <takanoda2005@gmail.com>
2025-09-22 15:32:08 +0900

    add props

--- app/timer/timer.tsx
+++ app/timer/timer.tsx
@@ -5,13 +5,12 @@ import {addScore} from "../sql/sql";
 
 let add_checker = 0;
 
-export function Timer({gameover}){
+export function Timer({gameover}: boolean){
     const [timer, setTimer] = useState(0);
         //sleep関数を設定
     const sleep = (ms:number) => new Promise(resolve => setTimeout(resolve, ms));
     useEffect(() => {
         (async () => {
-            console.log("今は" + gameover)
             if(gameover){
                 console.log("ブレーク");
                 if(add_checker == 0){
